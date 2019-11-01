758c0faf27ec8fd78a010f97e8fb1bc8de53015a
Scoop <trevorjburni@gmail.com>
2019-10-31 18:12:21 -0600

    I added some todo's in the form of comments to my javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// Psuedocode and steps for the javascript portion of this project
+
+// Create an array of farm crops
+
+// Create a function of sorts that selects a random number between 0 and the length of the array
+
